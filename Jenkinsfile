cb1768ffa7798832a644c59c00b30bf11708f1b8
CptSparrow <dinethsasanka2002@gmail.com>
2025-08-06 02:43:59 +0530

    Add a js file

---
+++ Jenkinsfile
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo '🔨 Building the project...'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                echo '✅ Running tests...'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo '🚀 Deploying the application...'
+            }
+        }
+    }
+}
---
+++ restaurant.js
@@ -0,0 +1,11 @@
+ window.alert("Welcome to Food Corner! Enjoy your meal and have a great day!");
+        console.log("Welcome to Food Corner! Enjoy your meal and have a nice day!");
+        document.addEventListener("DOMContentLoaded", function() {
+            const contactForm = document.getElementById("contactForm");
+            contactForm.addEventListener("submit", function(event) {
+                event.preventDefault();
+                alert("Thank you for contacting us! We will get back to you soon.");
+            });
+        });
+
+    
